86bf37e303673bc69dd4240aaa6ee5fb7c897306
Earl Harris <earleharrisjr@gmail.com>
2023-02-23 11:55:03 -0500

    create ticket controller

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,18 +3,20 @@
     public class Ticket
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string AgentName { get; set; }
+        public string ClientName { get; set; }
         public string Issue { get; set; }
         public string Details { get; set; }
-        public DateTime GetDate { get; set; }
+        public DateTime SubmittedDateTime { get; set; }
 
 
-        public Ticket(string name, string issue, string details, DateTime getdate)
+        public Ticket(string agentname, string clientname string issue, string details, DateTime submitteddatetime)
         {
-            Name = name;
+            AgentName = agentname;
+            ClientName = clientname;
             Issue = issue;
             Details = details;
-            GetDate = getdate;
+            SubmittedDateTime = submitteddatetime;
 
         }
     }
