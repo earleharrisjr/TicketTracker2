b07f9a8a1396bd101e700604f1b774e3d167d884
Earl Harris <earleharrisjr@gmail.com>
2023-02-23 11:42:40 -0500

    first commit/ create model for ticket]

---
+++ Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿namespace TicketTracker2.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Issue { get; set; }
+        public string Details { get; set; }
+        public DateTime GetDate { get; set; }
+
+
+        public Ticket(string name, string issue, string details, DateTime getdate)
+        {
+            Name = name;
+            Issue = issue;
+            Details = details;
+            GetDate = getdate;
+
+        }
+    }
+}
